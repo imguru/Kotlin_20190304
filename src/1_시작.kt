caf35dfa47c5c47cd0020994a99ce3a56d6581ef
Chansik Yun <chansigi@gmail.com>
2019-03-04 09:49:21 +0900

    .

--- src/1_시작.kt
+++ src/1_시작.kt
@@ -1,2 +1,28 @@
 // 1_시작
+// 1) 코틀린 언어 특징
+//  - 간결함
+//    : 보일러플레이트를 제거합니다.
+//  - 안전함
+//    : Null 포인터를 안전하게 다루는 기능을 제공합니다.
+//  - 상호 운용성
+//    : Java로 작성된 코드를 아무 설정없이 사용할 수 있다.
+//      Kotlin으로 작성된 코드를 자바에서 아무 설정없이 사용할 수 있다.
 
+fun main() {
+    println("Hello, Kotlin")
+}
+
+/*
+fun main(args: Array<String>) {
+    println("Hello, Kotlin")
+}
+*/
+
+/*
+// Sample.java
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java");
+    }
+}
+*/
