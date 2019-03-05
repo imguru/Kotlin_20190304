1a833f82a6a41f0981c1e1e22393347cb9ba002b
Chansik Yun <chansigi@gmail.com>
2019-03-05 15:17:13 +0900

    .

--- src/14_확장함수.kt
+++ src/14_확장함수.kt
@@ -1,4 +1,7 @@
 package ex14
+
+import java.lang.StringBuilder
+
 // 14_확장함수.kt
 //   => Extension Function
 //   => 코틀린 라이브러리의 핵심!
@@ -32,7 +35,11 @@ class MyString: String() {
 
 // Image img
 // img.glide.processing()
+// String. : 수신 객체 타입
+//   this  : 수신 객체 참조
+/*
 fun String.lastChar(): Char = this[this.length - 1]
+// private / protected 필드는 접근이 불가합니다.
 
 fun main() {
     val str = "Hello"
@@ -42,6 +49,48 @@ fun main() {
     // println(lastChar(str))
     println(str.lastChar())
 }
+*/
+
+// 1 2 3 -> joinToString -> [1, 2, 3]
+
+// 아래의 함수를 Collection<T>라는 타입에 대해서 확장하고 싶다.
+fun <T> joinToString(
+    collection: Collection<T>,
+    separator: String = ", ",
+    prefix: String = "",
+    postfix: String = ""
+): String {
+    val result = StringBuilder(prefix)
+
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0)
+            result.append(separator)
+        result.append(element)
+    }
+
+    result.append(postfix)
+    return result.toString()
+}
+
+fun main() {
+    val list = listOf(10, 20, 30)
+    val result = joinToString(
+        collection = list,
+        separator = ", ",
+        prefix = "[",
+        postfix = "]"
+    )
+
+    println(result)
+}
+
+
+
+
+
+
+
+
 
 
 
