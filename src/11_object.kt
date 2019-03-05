2675d97bd3b439f91c6eaa74216299652f608f33
Chansik Yun <chansigi@gmail.com>
2019-03-05 09:51:11 +0900

    .

--- src/11_object.kt
+++ src/11_object.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 // 11_object.kt
 
 // 1. object declaration
@@ -26,6 +28,70 @@ object Cursor {
     }
 }
 
+/*
+class CaseInsensitiveFileComparator : Comparator<File> {
+    override fun compare(o1: File, o2: File): Int {
+        return o1.path.compareTo(o2.path, ignoreCase = true)
+    }
+}
+*/
+
+// - 상속 또는 인터페이스를 구현하는 것도 가능합니다.
+object CaseInsensitiveFileComparator : Comparator<File> {
+    override fun compare(o1: File, o2: File): Int {
+        return o1.path.compareTo(o2.path, ignoreCase = true)
+    }
+}
+
+data class Person(val name: String, val level: Int) {
+    object NameComparator: Comparator<Person> {
+        override fun compare(o1: Person, o2: Person): Int {
+            return o1.name.compareTo(o2.name)
+        }
+    }
+
+    object LevelComparator: Comparator<Person> {
+        override fun compare(o1: Person, o2: Person): Int {
+            return o1.level.compareTo(o2.level)
+        }
+    }
+}
+
 fun main(args: Array<String>) {
+    val people = listOf(Person("Bob", 15),
+        Person("Tom", 3))
+
+    println(people.sortedWith(Person.NameComparator))
+    println(people.sortedWith(Person.LevelComparator))
+
+
+
+    val files = listOf(File("/Z"), File("/a"))
+
+    // val sorted = files.sortedWith(CaseInsensitiveFileComparator())
+    // println(sorted)
+
+     val sorted = files.sortedWith(CaseInsensitiveFileComparator)
+     println(sorted)
+
+
     Cursor.move(10, 20)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
