4a38b9885fd7cd38d16bf19a1a245a8cb3cff776
Chansik Yun <chansigi@gmail.com>
2019-03-06 15:08:33 +0900

    .

--- src/24_메모이제이션2.kt
+++ src/24_메모이제이션2.kt
@@ -1,7 +1,18 @@
 package ex24_2
 
 import com.google.gson.GsonBuilder
-import sun.jvm.hotspot.debugger.Address
+
+// 기존 함수에 메모이제이션을 적용하는 함수
+
+fun <A, R> ((A)->R).memoized(): (A) -> R {
+    val map = mutableMapOf<A, R>()
+    return { k ->
+        map.getOrPut(k) {
+            this(k)
+        }
+    }
+}
+
 
 // Flyweight
 class User(val name: String, val age: Int, val address: String)
@@ -23,8 +34,12 @@ fun main() {
         User("Tom", 42, "")
     )
 
+    // 기존 함수에 메모이제이션을 적용해서 새로운 함수를 만들었습니다.
+    val toJson = User::toJson.memoized()
+
     for (e in users) {
-        val json = e.toJson()
+        // val json = e.toJson()
+        val json = toJson(e)
         println(json)
     }
 }
