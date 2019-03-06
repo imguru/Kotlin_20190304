4a3db14da9b263bcc5daecdda259a23749feff0a
Chansik Yun <chansigi@gmail.com>
2019-03-06 10:20:04 +0900

    .

--- src/19_커링.kt
+++ src/19_커링.kt
@@ -1,3 +1,6 @@
+import java.lang.Appendable
+import java.time.LocalDateTime
+
 // 19_커링.kt
 
 // 커링(Currying)
@@ -30,6 +33,20 @@ fun diff(x: Int, y: Int) = x - y
 //    x + y
 //}
 
+/*
+fun main() {
+     val csum = ::sum.curried()
+     println(csum(10)(20))
+
+    val csum3 = ::sum3.curried()
+    val result = csum3(10)(20)(30)
+
+    println(result)
+}
+*/
+
+
+
 // 인자가 2개인 함수에 대해서 currying 지원을 추가해 봅시다.
 fun <P1, P2, R> ((P1, P2) -> R).curried(): (P1) -> (P2) -> R = { p1 ->
     { p2 ->
@@ -45,15 +62,38 @@ fun <P1, P2, P3, R> ((P1, P2, P3) -> R).curried(): (P1) -> (P2) -> (P3) -> R = {
     }
 }
 
-fun main() {
-     val csum = ::sum.curried()
-     println(csum(10)(20))
+// 2) 부분 적용
+//  => 함수의 인자를 고정해서 새로운 함수를 생성하는 기법
+enum class Level { INFO, WARN, ERROR, FATAL }
 
-    val csum3 = ::sum3.curried()
-    val result = csum3(10)(20)(30)
-
-    println(result)
+// 로깅 라이브러리
+fun log(level: Level, appendable: Appendable, message: String) {
+    appendable.appendln("[${level.name}]:[${LocalDateTime.now()}]: $message")
 }
+
+// 다른 라이브러리
+fun compute(logger: ((String) -> Unit)? = null) {
+    logger?.invoke("compute 함수 시작")
+    logger?.invoke("compute 동작 수행")
+    logger?.invoke("compute 함수 종료")
+}
+
+fun main() {
+    log(Level.INFO, System.out, "main 함수가 시작되었습니다.")
+
+    compute()
+    
+    // 람다
+    compute { message ->
+        log(Level.INFO, System.out, message)
+    }
+
+    // 부분 적용
+    val mylog = ::log.curried()(Level.INFO)(System.out)
+    compute(mylog)
+}
+
+
 
 
 
