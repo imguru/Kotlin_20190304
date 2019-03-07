7e5ff2bbacfa2002f17b2259348136a7362b7f0e
Chansik Yun <chansigi@gmail.com>
2019-03-07 15:31:29 +0900

    .

---
+++ android/SimpleGithub/app/src/main/java/xyz/ourguide/lge/simplegithub/MainActivity.kt
@@ -0,0 +1,47 @@
+package xyz.ourguide.lge.simplegithub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// SSL - proxy
+// mLGE: 4f3d2s1a@@
+
+// SimpleGithubApp
+//   => Retrofit
+//         implementation "com.squareup.retrofit2:retrofit:$retrofit_version"
+//         implementation "com.squareup.retrofit2:converter-gson:$retrofit_version"
+
+//      OkHttp
+//         implementation "com.squareup.okhttp3:okhttp:$okhttp_version"
+//         implementation "com.squareup.okhttp3:logging-interceptor:$okhttp_version"
+
+//      Glide - Image Processing Library
+//        apply plugin: 'kotlin-kapt'
+
+//        implementation "com.github.bumptech.glide:glide:4.9.0"
+//        annotationProcessor "com.github.bumptech.glide:compiler:4.9.0" // Java
+//        kapt "com.github.bumptech.glide:compiler:4.9.0"                // Kotlin
+
+//      RxJava / RxAndroid
+//        implementation "io.reactivex.rxjava2:rxjava:2.2.7"
+//        implementation 'io.reactivex.rxjava2:rxandroid:2.1.1'
+
+//      RxKotlin
+//        implementation 'io.reactivex.rxjava2:rxkotlin:2.3.0'
+
+//      RxBinding
+//        implementation 'com.jakewharton.rxbinding3:rxbinding:3.0.0-alpha2'
+//        implementation 'com.jakewharton.rxbinding3:rxbinding-core:3.0.0-alpha2'
+//        implementation 'com.jakewharton.rxbinding3:rxbinding-recyclerview:3.0.0-alpha2'
+//        implementation 'com.jakewharton.rxbinding3:rxbinding-appcompat:3.0.0-alpha2'
+
+//      Gson
+//        implementation 'com.google.code.gson:gson:2.8.5'
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
